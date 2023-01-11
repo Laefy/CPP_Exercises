d599bcab9b057e4ec0e4568096c19ee2498d0500
Marc Agnetti <agnettim.pro@gmail.com>
2023-01-11 13:39:23 +0100

    Helloworld with string + gitignore on ds_store

--- TP1/helloworld.cpp
+++ TP1/helloworld.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
+    std::cout << "What's your name?" << std::endl;
 
-    auto var = 0;
+    // std::cout << "Hello world!" << std::endl;
+
+    // auto var = 0;
+    std::string var;
     std::cin >> var;
+
+    std::cout << "Hello " << var << "!" << std::endl;
 
     return 0;
 }
