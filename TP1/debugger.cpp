8168c8cedfaaeb8a2e58dada9f1c65f7119d1fe7
Atrax Nicolas <nicolas.atrax9@gmail.com>
2023-01-12 14:53:09 +0100

    TP1 Completed

---
+++ TP1/debugger.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    using namespace std;
+
+    string var;
+    cout << "Quel est votre nom ?" << endl;
+    cin >> var;
+    cout << "Enchanté " << var << " !" << endl;
+    return 0;
+}
