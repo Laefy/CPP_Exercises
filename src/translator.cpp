8017c9224e6d5de0fb751e7b47fd16e93933e794
Céline Noël <c.noel@dental-monitoring.com>
2023-01-16 22:32:38 +0100

    Question 1.

--- src/translator.cpp
+++ src/translator.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
+#include <set>
+#include <string>
+
+std::set<std::string> make_exit_commands()
+{
+    return { "q", "e", "exit", "quit" };
+}
 
 int main()
 {
+    const auto exit_commands = make_exit_commands();
+
     while (true)
     {
-        std::cout << "This is a translator, but it does nothing for now..." << std::endl;
-        std::cin.ignore();
+        std::cout << "Enter a command: " << std::endl;
+
+        auto command = std::string {};
+        std::cin >> command;
+
+        if (exit_commands.count(command) != 0)
+        {
+            break;
+        }
+
+        std::cout << std::endl;
     }
 
     return 0;
