ff47e74572b48bf2843cabd6f8b413a94188bd04
Céline Noël <c.noel@dental-monitoring.com>
2023-02-08 15:55:36 +0100

    Update test 1c. in TP4.

--- tests/Test-1.cpp
+++ tests/Test-1.cpp
@@ -23,16 +23,17 @@ TEST_CASE("1b. Querying the name of a Pokemon does not modify the Pokemon")
 TEST_CASE("1c. Whenever a Pokemon is constructed, it is assigned an identifier that gets incremented each time.")
 {
     const auto pikachu = Pokemon { "Pikachu" };
+    const auto first_id = pikachu.id();
+
     const auto bulbizarre = Pokemon { "Bulbizarre" };
     const auto pikachu_copy = pikachu;
 
-    REQUIRE(pikachu.id() == 0);
-    REQUIRE(bulbizarre.id() == 1);
-    REQUIRE(pikachu_copy.id() == 2);
+    REQUIRE(bulbizarre.id() == first_id + 1);
+    REQUIRE(pikachu_copy.id() == first_id + 2);
 
     auto pikachu_becoming_bulbizarre = pikachu;
-    REQUIRE(pikachu_becoming_bulbizarre.id() == 3);
-    
+    REQUIRE(pikachu_becoming_bulbizarre.id() == first_id + 3);
+
     pikachu_becoming_bulbizarre = bulbizarre;
-    REQUIRE(pikachu_becoming_bulbizarre.id() == 3);
+    REQUIRE(pikachu_becoming_bulbizarre.id() == first_id + 3);
 }
