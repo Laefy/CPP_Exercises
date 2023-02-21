7193e7e2b8254e5bb2fe5164e22db6dafbd5a214
Victor Marsault <marsault@mercure>
2023-02-21 16:25:03 +0100

    Typo in test10

--- tests/test10-make-ptr.cpp
+++ tests/test10-make-ptr.cpp
@@ -34,17 +34,17 @@ TEST_CASE("ObjectNode::make_ptr()")
 
 TEST_CASE("Note that all these member-functions return something that can be stored in std::unique_ptr<Node>.")
 {
-    std::unique_ptr<Node> node_ptr = IntLeaf::make_ptr(1);
-    REQUIRE(node_ptr->print() == "1");
+    std::unique_ptr<Node> node_ptr1 = IntLeaf::make_ptr(1);
+    REQUIRE(node_ptr1->print() == "1");
 
-    std::unique_ptr<Node> node_ptr = StringLeaf::make_ptr("Hello world");
-    REQUIRE(node_ptr->print() == "\"Hello world\"");
+    std::unique_ptr<Node> node_ptr2 = StringLeaf::make_ptr("Hello world");
+    REQUIRE(node_ptr2->print() == "\"Hello world\"");
 
-    std::unique_ptr<Node> node_ptr = ArrayNode::make_ptr();
-    REQUIRE(node_ptr->print() == "[]");
+    std::unique_ptr<Node> node_ptr3 = ArrayNode::make_ptr();
+    REQUIRE(node_ptr3->print() == "[]");
 
-    std::unique_ptr<Node> node_ptr = ObjectNode::make_ptr();
-    REQUIRE(node_ptr->print() == "{}");
+    std::unique_ptr<Node> node_ptr4 = ObjectNode::make_ptr();
+    REQUIRE(node_ptr4->print() == "{}");
 }
 
 #include "routine_memory_check.cpp"
