d5293c6a88b319dafa2fb3535573b41111f6e691
Victor Marsault <marsault@mercure>
2023-02-23 20:11:35 +0100

    Small problem in InstanceCounter

--- src/InstanceCounter.cpp
+++ src/InstanceCounter.cpp
@@ -1,10 +1,3 @@
 #include "InstanceCounter.hpp"
 
 int InstanceCounter::_counter = 0;
-
-InstanceCounter& InstanceCounter::operator=(const InstanceCounter& other)
-{
-    if (this != &other)
-        ++_counter;
-    return *this;
-}
