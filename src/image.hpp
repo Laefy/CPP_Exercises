d0512bb2c60cafc5e078415d1168af880828f2f1
clement <clement.chomicki@protonmail.com>
2023-03-25 16:48:01 +0000

    correction

--- src/image.hpp
+++ src/image.hpp
@@ -11,3 +11,125 @@
 #include <array>
 #include <iostream>
 
+
+template<typename P, size_t W, size_t H>
+struct Image
+{
+private:
+    std::array<std::array<P, W>, H> _data;
+public:
+    Image(const P & pix)
+    {
+        for (size_t j = 0; j < H; ++j)
+        {
+            for (size_t i = 0; i < W; ++i)
+            {
+                _data[j][i] = pix;
+            }
+        }
+    }
+
+    Image() = default;
+
+    const P &
+    operator()(const size_t i,
+               const size_t j) const
+    {
+        return _data[j][i];
+    }
+    P &
+    operator()(const size_t i,
+               const size_t j)
+    {
+        return _data[j][i];
+    }
+
+    Image(const std::function<P(const size_t, const size_t)> & foo)
+    {
+        for (size_t j = 0; j < H; ++j)
+        {
+            for (size_t i = 0; i < W; ++i)
+            {
+                (*this)(i,j) = foo(i,j);
+            }
+        }        
+    }
+
+    Image<P, W, H>(const std::string & path)
+{
+    int w, h, c;
+    const uint8_t * data = stbi_load(path.c_str(),
+                                     &w, &h, &c, 0);
+    if(data == NULL)
+    {
+        std::cout << "Could not find image '" << path << "'" << std::endl;
+        exit(1);
+    }
+    else if (w!=W || h != H || c != sizeof(P))
+    {
+        std::cout << "Could not open image '"
+                  << path << "' with the dimensions ("
+                  << W <<"," << H << ","
+                  << sizeof(P)<<"), found ("
+                  << w <<","<<h<<","<< c  << ")" << std::endl;
+    }
+    else
+    {
+        for (size_t i = 0; i < W; ++i)
+        {
+            for (size_t j = 0; j < H; ++j)
+            {
+                const uint8_t * pixel_ptr = data + ((j%h)*w + (i%w))*c;
+                (*this)(i,j) = *reinterpret_cast<const P *>(pixel_ptr);
+            }
+        }
+    }
+}
+
+void
+save(const std::string & path) const
+{
+    const auto data = reinterpret_cast<const uint8_t *>(this);
+    stbi_write_png(path.c_str(), W, H, sizeof(P), data, W*sizeof(P));
+}
+    
+
+};
+
+
+
+template<typename A, typename B, size_t W, size_t H>
+Image<A, W, H>
+operator+(const Image<A,W,H> & lhs,
+          const Image<B,W,H> & rhs)
+{
+    Image<A, W, H> out = {};
+    for (size_t j = 0; j < H; ++j)
+    {
+        for (size_t i = 0; i < W; ++i)
+        {
+            out(i,j) = lhs(i,j) + rhs(i,j);
+        }
+    }
+    return out;
+}
+
+
+
+
+
+template<typename A, size_t W, size_t H>
+Image<RGBA, W, H>
+operator+(const Image<A,W,H> & lhs,
+          const Image<Luma,W,H> & rhs)
+{
+    Image<RGBA, W, H> out = {};
+    for (size_t j = 0; j < H; ++j)
+    {
+        for (size_t i = 0; i < W; ++i)
+        {
+            out(i,j) = lhs(i,j) + rhs(i,j);
+        }
+    }
+    return out;
+}
--- src/pixels.hpp
+++ src/pixels.hpp
@@ -5,3 +5,95 @@
 #include <stdint.h>
 
 
+struct RGBA
+{
+    uint8_t r,g,b,a;
+};
+struct RGB
+{
+    uint8_t r,g,b;
+};
+struct Luma
+{
+    uint8_t gray;
+};
+
+
+template<typename A, typename B>
+A from(const B & x)
+{
+    return from<A, RGBA>(from<RGBA, B>(x));
+}
+    
+template<>
+RGBA from(const RGBA & x)
+{
+    return x;
+}
+    
+template<>
+RGBA from(const RGB & x)
+{
+    return {x.r,x.g,x.b,255};
+}
+
+template<>
+RGBA from(const Luma & x)
+{
+    const auto [r,g,b] = lib::grayscale_to_rgb(x.gray);
+    return from<RGBA, RGB>({r,g,b});
+}
+template<>
+RGB from(const RGBA & x)
+{
+    return {x.r,x.g,x.b};
+}
+
+template<>
+Luma from(const RGBA & x)
+{
+    return {lib::rgb_to_grayscale(x.r,x.g,x.b)};
+}
+
+
+template<typename A, typename B>
+B into(const A & x)
+{
+    return from<B,A>(x);
+}
+
+RGBA
+operator+(const RGBA & lhs,
+          const RGBA & rhs)
+{
+    const auto [r,g,b,a] = lib::mix_color(
+        lhs.r, lhs.g, lhs.b, lhs.a,
+        rhs.r, rhs.g, rhs.b, rhs.a
+        );
+    return {r,g,b,a};
+}
+
+#define IMPL_BLEND(A,B,C)                       \
+C operator+(const A & lhs, const B & rhs)       \
+{                                               \
+    return from<C, RGBA>(from<RGBA, A>(lhs)     \
+                         + from<RGBA, B>(rhs)); \
+}
+
+IMPL_BLEND(RGBA, RGB , RGBA);
+IMPL_BLEND(RGB , RGBA, RGB );
+IMPL_BLEND(Luma, RGBA, RGBA);
+IMPL_BLEND(Luma, RGB , RGB);
+IMPL_BLEND(RGB , RGB , RGB);
+
+
+// mask operator
+template<typename Pix>
+RGBA
+operator+(const Pix & pix,
+          const Luma & mask)
+{
+    RGBA rgba = from<RGBA, Pix>(pix);
+    rgba.a = uint8_t((uint32_t(rgba.a)*uint32_t(mask.gray)/255));
+    return rgba;
+}
