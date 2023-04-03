5ffa2be769f74474a64f1bd5fd0aa73d5a5fa5f1
Victor Marsault <marsault@mercure>
2023-04-03 11:12:27 +0200

    Spli test11 in 2

---
+++ tests/ex1/test12-pairwise-concat1.cpp
@@ -0,0 +1,26 @@
+#include "../../src/ex1.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+#include <list>
+
+/*
+Your job is to write (in files ex1.{hpp,cpp}) a function pairwise_concatenate(l1,l2) that
+- takes as argument two list of lists l1 and l2
+- **copies** the lists inside l1 to the lists inside l2 using `concatenate`
+  the content of l2[i] is moved into l1[i] for each i.
+*/
+
+TEST_CASE("a. Pairwise_concatenate with an l-value reference")
+{
+    std::list<std::list<int>> list1 = { { 0, 1, 2 }, { 3, 4 }, { 5, 6 } };
+    std::list<std::list<int>> list2 = { { 0, 1, 2 }, { 3, 4 }, { 5, 6 } };
+
+    pairwise_concatenate(list1, list2);
+    //                          ^^^^^ l-value reference
+
+    std::list<std::list<int>> target1 = { { 0, 1, 2, 0, 1, 2 }, { 3, 4, 3, 4 }, { 5, 6, 5, 6 } };
+    REQUIRE(list1 == target1);
+
+    std::list<std::list<int>> target2 = { { 0, 1, 2 }, { 3, 4 }, { 5, 6 } };
+    REQUIRE(list2 == target2);
+}
