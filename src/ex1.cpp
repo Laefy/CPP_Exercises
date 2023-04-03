f2196d30512b9019013f0e8131322f9c6b9ddb10
Victor Marsault <marsault@mercure>
2023-04-03 11:12:01 +0200

    In line with new tests10-12

--- src/ex1.cpp
+++ src/ex1.cpp
@@ -1,14 +1,27 @@
 #include "ex1.hpp"
 
-void pairwise_splice(std::list<std::list<int>> &list1, std::list<std::list<int>> &&list2)
-{
+#include "../lib/concatenate.hpp"
 
+void pairwise_concatenate(std::list<std::list<int>>& list1, std::list<std::list<int>>&& list2)
+{
     for (unsigned i = list1.size(); i < list2.size(); ++i)
         list1.emplace_back();
     auto it1 = list1.begin();
     for (auto it2 = list2.begin(); it2 != list2.end(); ++it2)
     {
-        it1->splice(it1->end(), std::move(*it2));
+        concatenate(*it1, std::move(*it2));
+        ++it1;
+    }
+}
+
+void pairwise_concatenate(std::list<std::list<int>>& list1, const std::list<std::list<int>>& list2)
+{
+    for (unsigned i = list1.size(); i < list2.size(); ++i)
+        list1.emplace_back();
+    auto it1 = list1.begin();
+    for (auto it2 = list2.begin(); it2 != list2.end(); ++it2)
+    {
+        concatenate(*it1, *it2);
         ++it1;
     }
 }
--- src/ex1.hpp
+++ src/ex1.hpp
@@ -2,4 +2,5 @@
 
 #include <list>
 
-void pairwise_splice(std::list<std::list<int>> &list1, std::list<std::list<int>> &&list2);
+void pairwise_concatenate(std::list<std::list<int>>& list1, std::list<std::list<int>>&& list2);
+void pairwise_concatenate(std::list<std::list<int>>& list1, const std::list<std::list<int>>& list2);
