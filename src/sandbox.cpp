c186833ceed3068a3a41d047105a2faa35b2a70e
Victor Marsault <marsault@mercure>
2023-04-03 09:01:37 +0200

    Style

--- src/sandbox.cpp
+++ src/sandbox.cpp
@@ -1,5 +1,4 @@
 #include <string>
 
 int main()
-{
-}
+{}
--- src/uge_unique_ptr.hpp
+++ src/uge_unique_ptr.hpp
@@ -1,51 +1,56 @@
 #pragma once
+#include <memory>
 
 /* Please implement me without using `std::unique_ptr` */
-namespace uge
+namespace uge {
+template <typename T>
+class unique_ptr
 {
-    template <typename T>
-    class unique_ptr
+public:
+    unique_ptr()
+        : _ptr { nullptr }
+    {}
+    unique_ptr(T* ptr)
+        : _ptr { ptr }
+    {}
+    unique_ptr(const unique_ptr<T>& other)
     {
-    public:
-        unique_ptr() : _ptr{nullptr} {}
-        unique_ptr(T *ptr) : _ptr{ptr} {}
-        unique_ptr(const unique_ptr<T> &other)
-        {
-            if (other.get() != nullptr)
-                _ptr = new T{*(other._ptr)};
-            else
-                _ptr = nullptr;
-        }
+        if (other.get() != nullptr)
+            _ptr = new T { *(other._ptr) };
+        else
+            _ptr = nullptr;
+    }
 
-        unique_ptr(unique_ptr<T> &&other) : _ptr{other._ptr} { other._ptr = nullptr; }
+    unique_ptr(unique_ptr<T>&& other)
+        : _ptr { other._ptr }
+    {
+        other._ptr = nullptr;
+    }
 
-        unique_ptr<T> &operator=(T *ptr)
-        {
-            if (_ptr != nullptr)
-                delete _ptr;
-            _ptr = ptr;
-            return (*this);
-        }
-
-        T &
-        operator*()
-        {
-            return *_ptr;
-        }
-
-        T *get() const
-        {
-            return _ptr;
-        }
-
-        ~unique_ptr()
+    unique_ptr<T>& operator=(uge::unique_ptr<T>&& other)
+    {
+        if (this != &other)
         {
             if (_ptr != nullptr)
                 delete _ptr;
+            _ptr       = other._ptr;
+            other._ptr = nullptr;
         }
+        return (*this);
+    }
 
-    private:
-        T *_ptr;
-    };
+    T& operator*() { return *_ptr; }
 
-}
+    T* get() const { return _ptr; }
+
+    ~unique_ptr()
+    {
+        if (_ptr != nullptr)
+            delete _ptr;
+    }
+
+private:
+    T* _ptr;
+};
+
+} // namespace uge
