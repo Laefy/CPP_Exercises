284eb93499b023df63778fd449a09e0af4ea7c47
maxime.jaillard <maxime.jaillard@orange.fr>
2024-01-19 11:00:57 +0100

    tp1 - ex1 - q6

--- tp1/ex1/main.cpp
+++ tp1/ex1/main.cpp
@@ -1,12 +1,13 @@
 #include <iomanip>
 #include <iostream>
+#include <string>
 
 int main()
 {
     std::cout << "Entre ton nom: ";
 
-    char name[20] = "";
-    std::cin >> std::setw(20) >> name;
+    std::string name = "";
+    std::cin >> name;
     std::cout << "Bonjour " << name << " !" << std::endl;
 
     return 0;
