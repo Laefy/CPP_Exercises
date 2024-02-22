3ce8f97a583ce47628a2d818ff07ed9dc3bc3eaa
Celine Noel <laefy22@gmail.com>
2024-02-22 23:39:59 +0100

    Split TP5 in two.

--- tp5/3-recipes/Material.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <ostream>
-
-class Material
-{
-    // Affiche le nom d'un materiau.
-    friend std::ostream& operator<<(std::ostream& stream, const Material& material) { return stream; }
-};
--- tp5/3-recipes/Recipe.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <ostream>
-
-class Recipe
-{
-    // Affiche l'identifiant et la formule d'une recette.
-    friend std::ostream& operator<<(std::ostream& stream, const Recipe& recipe) { return stream; }
-};
