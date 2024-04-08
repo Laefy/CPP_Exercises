a5faa9ad0e890973dbba5ad18f94843e655a06df
Celine Noel <laefy22@gmail.com>
2024-04-08 12:33:03 +0200

    Update Ex1 in order to concentrate on the template / functional part.

--- tp11/src/factory.cpp
+++ tp11/src/factory.cpp
@@ -1,7 +1,7 @@
-#include <memory>
-#include <string>
-#include <sstream>
 #include <iostream>
+#include <memory>
+#include <sstream>
+#include <string>
 #include <vector>
 
 // Liste des entitées à construire
@@ -24,37 +24,74 @@ class Factory
 public:
     // using Builder = ...;
 
-    // template <...>
-    void register_entity(...) {
-    }
+    template <typename TDerivedEntity>
+    void register_entity()
+    {}
 
-    std::unique_ptr<Entity> build(const std::string& id) const
-    {
-        return nullptr;
-    }
+    std::unique_ptr<Entity> build(const std::string& id) const { return nullptr; }
 
 private:
+    // ...
 };
 
-class Object: public Entity
+class Object : public Entity
 {
 public:
-    void print() const override
-    {
-        std::cout << "Object" << std::endl;
-    }
+    void print() const override { std::cout << "Object" << std::endl; }
 };
 
-class Tree : public Object {};
+class Tree : public Object
+{
+public:
+    void print() const override { std::cout << "Tree" << std::endl; }
+};
 
-class Person : public Entity {};
-class Animal : public Entity {};
-class House : public Object {};
+class Person : public Entity
+{
+public:
+    explicit Person(std::string name)
+        : _name { std::move(name) }
+    {}
+
+    void print() const override { std::cout << _name << std::endl; }
+
+    void               set_name(std::string name) { _name = std::move(name); }
+    const std::string& get_name() const { return _name; }
+
+private:
+    std::string _name;
+};
+
+class Animal : public Entity
+{
+public:
+    explicit Animal(std::string species)
+        : _species { std::move(species) }
+    {}
+
+    void print() const override { std::cout << _species << std::endl; }
+
+private:
+    std::string _species;
+};
+
+class House : public Object
+{
+public:
+    explicit House(Person& owner)
+        : _owner { owner }
+    {}
+
+    void print() const override { std::cout << "House owned by " << _owner.get_name() << std::endl; }
+
+private:
+    Person& _owner;
+};
 
 int main()
 {
     Factory factory;
-    //factory.register_entity<Object>("Object");
+    // factory.register_entity<Object>("Object");
 
     std::vector<std::unique_ptr<Entity>> entities;
 
@@ -68,7 +105,7 @@ int main()
         entities.emplace_back(factory.build(e));
     }
 
-    for (const auto& e: entities)
+    for (const auto& e : entities)
     {
         if (e != nullptr)
         {
@@ -78,5 +115,3 @@ int main()
 
     return 0;
 }
-
-
