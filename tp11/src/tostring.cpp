7f15706ed9d1f9a31e1c6867e55a4c7f6027bf7a
Henri Derycke <henri.derycke@ens-lyon.org>
2024-04-11 10:03:33 +0200

    tp11: improve tostring tests

--- tp11/src/tostring.cpp
+++ tp11/src/tostring.cpp
@@ -38,37 +38,45 @@ public:
 class Both : public Streamable, public Convertible
 {};
 
+template <typename T>
+void print_test(std::string type, T&& value) {
+    std::cout << type << std::endl;
+    std::cout << "** Error: value is not a std::string" << std::endl;
+}
+
+void print_test(std::string type, std::string value)
+{
+    std::cout << type << std::endl;
+    std::cout << " -> " << value << std::endl;
+}
+
 int main()
 {
     // De base on a l'adresse des objets
-    std::cout << "Vide" << std::endl << " -> " << to_string(Empty {}) << std::endl;
+    print_test("Vide", to_string(Empty {}));
 
     // Q1: on veut gérer les chaines de caractères
-    std::cout << "std::string" << std::endl << " -> " << to_string(std::string { "Machin" }) << std::endl;
-    std::cout << "char*" << std::endl << " -> " << to_string("Bidule") << std::endl;
+    print_test("std::string", to_string(std::string { "Machin" }));
+    print_test("char*", to_string("Bidule"));
 
     // Q2: Puis les conversions simples fournies par la fonction std::to_string
-    std::cout << "int" << std::endl << " -> " << to_string(3) << std::endl;
+    print_test("int", to_string(3));
 
     // Q4: On s'attaque maintenant aux conteneurs
-    std::cout << "std::vector<int>" << std::endl
-              << " -> " << to_string(std::vector<int> { 1, 2, 3, 4 }) << std::endl;
-    std::cout << "std::vector<std::vector<int>>" << std::endl
-              << " -> "
-              << to_string(std::vector<std::vector<int>> { std::vector<int> { 0, 1, 2, 3 },
-                                                           std::vector<int> { 4, 5, 6, 7 } })
-              << std::endl;
+    print_test("std::vector<int>", to_string(std::vector<int> { 1, 2, 3, 4 }));
+    print_test("std::vector<std::vector<int>>",
+               to_string(std::vector<std::vector<int>> { std::vector<int> { 0, 1, 2, 3 },
+                                                         std::vector<int> { 4, 5, 6, 7 } }));
     // Q4b: avec une spécialisation pour les std::array
-    std::cout << "std::array<int, 4>" << std::endl
-              << " -> " << to_string(std::array<int, 4> { 1, 2, 3, 4 }) << std::endl;
+    print_test("std::array<int, 4>", to_string(std::array<int, 4> { 1, 2, 3, 4 }));
 
     // Q5: Si un objet présente une fonction de conversion, on l'utilise
-    std::cout << "Convertible" << std::endl << " -> " << to_string(Convertible {}) << std::endl;
+    print_test("Convertible", to_string(Convertible {}));
 
     // Q6: S'il est streamable (en implémentant l'opérateur << sur ostream), idem
-    std::cout << "Streamable" << std::endl << " -> " << to_string(Streamable {}) << std::endl;
+    print_test("Streamable", to_string(Streamable {}));
 
     // Commenter cette ligne si elle bloque la compilation trop tot dans le TP
-    std::cout << "Both" << std::endl << " -> " << to_string(Both {}) << std::endl;
+    print_test("Both", to_string(Both {}));
     return 0;
 }
